83a2283e1e773b3cbdcebd056f913936b6860362
ahmed_hefow <axmedaadan484@gmail.com>
2025-05-07 10:03:45 +0300

    updated

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import userRoutes from './routes/userRoutes.js'
 import driverRoutes from './routes/driverRoutes.js'
 import RideRequestRoutes from './routes/RideRequestRoutes.js'
 import authRoutes from './routes/authRoutes.js'
+import otpRoutes from './routes/twilioRoutes.js'
 
 dotenv.config();
 connectDB();
@@ -14,10 +15,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
+app.use('/api/otp', otpRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use('/api/driver',driverRoutes);
 app.use('/api/rides', RideRequestRoutes); // Add this line
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`the server is run on port number  ${PORT}`));
--- mobile_app/lib/main.dart
+++ mobile_app/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'SAFARX',
+      title: 'SafarX',
       home: const SplashScreen(),
     );
   }
